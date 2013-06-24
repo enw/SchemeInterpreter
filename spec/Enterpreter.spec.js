ac55875ad8a35aeb9a1cc414ec580c0a12f6aea3
Elliot Winard <github@caveteen.com>
2013-06-24 12:23:25 -0400

    add (failing) test for lambdas with sequence of lisp expressions

--- spec/Enterpreter.spec.js
+++ spec/Enterpreter.spec.js
@@ -94,6 +94,10 @@ describe('Enterpreter Suite', function () {
         function () {
             expect(evaluate('(( lambda ( x ) ( + x 3 )) 3)')).toBe(6); // add 3
         });
+    it('executes lambda expressions with sequences',
+        function () {
+            expect(evaluate('(( lambda ( x ) (define y 3) ( + y 3 )) 3)')).toBe(6); // add 3
+        });
     it('executes begin expressions',
         function () {
             expect(evaluate('(begin (define x 7) (define y 32) (+ x y))')).toBe(39);
